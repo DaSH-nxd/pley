2051d0cda913017f24ae88ce8d799c88f4577661
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-04-14 08:03:13 -0700

    add delete profiles and change passwords endpoints to user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -19,7 +19,7 @@ router.post(
         check('username', 'Please Enter a Valid Username').not().isEmpty(),
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter a valid password').isLength({
-            min : 6
+            min : 8
         }),
     ],
     async (req, res) => {
@@ -81,7 +81,7 @@ router.post(
 /**
  * @method - POST
  * @param - /user/login
- * @method - User Login
+ * @description - User Login
  */
 
 router.post(
@@ -89,7 +89,7 @@ router.post(
     [
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter a valid password').isLength({
-            min : 6,
+            min : 8,
         }),
     ],
     async (req, res) => {
@@ -149,18 +149,98 @@ router.post(
 
 /**
  * @method - GET
- * @description - Get LoggedIn User
  * @param - /user/me
+ * @description - Get LoggedIn User using token
  */
 
-router.get('/me', auth, async (req, res) => {
-    try {
-        // req.user is getting fetched from Middleware after token authentication
-        const user = await User.findById(req.user.id);
-        res.json(user);
-    } catch (e) {
-        res.send({ message : 'Error In Fetching User' });
+router.get(
+    '/me', 
+    auth, 
+    async (req, res) => {
+        try {
+            // req.user is getting fetched from Middleware after token authentication
+            const user = await User.findById(req.user.id);
+            res.json(user);
+        } catch (e) {
+            res.send({ message : 'Error In Fetching User' });
+        }
     }
-});
+);
+
+/**
+ * @method - PUT
+ * @param - /user/delete
+ * @description - Delete User if they want to delete their account, must delete with token
+ */
+
+router.put(
+    '/delete',
+    auth,
+    async (req, res) => {
+        try {
+            const deletedUser = await User.findByIdAndDelete(req.user.id);
+            if (!deletedUser) {
+                return res.status(400).json({
+                    msg : 'Could not Find User to Delete',
+                });
+            }
+            res.json(deletedUser);
+        } catch (e) {
+            res.send({ message : 'Error in Deleting User' });
+        }
+    }
+);
+
+/**
+ * @method - PUT
+ * @param - /user/password
+ * @description - Change password, must enter email to change password
+ */
+
+ router.put(
+    '/password',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('pass1', 'Please enter valid passwords').isLength({
+            min : 8,
+        }),
+        check('pass2', 'Please enter valid passwords').isLength({
+            min : 8,
+        }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors : errors.array(),
+            });
+        }
+
+        const { email, pass1, pass2 } = req.body;
+        try {
+            if (pass1 === pass2) {
+                const salt = await bcrypt.genSalt(10);
+                newPassword = await bcrypt.hash(pass1, salt);
+
+                userToChange = await User.findOneAndUpdate(
+                    {
+                        email,
+                    }, 
+                    {
+                        password : newPassword,
+                    },
+                );
+
+                res.send({ message : "Password Updated"});
+            } else {
+                return res.status(400).json({
+                    msg : "Passwords Don't Match",
+                });
+            }
+        } catch (e) {
+            res.send({ message : 'Could not find user' });
+        }
+    }
+);
 
 module.exports = router;
