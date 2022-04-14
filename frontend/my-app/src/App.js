32bd4720b6b8716f5ee6323b3e246b2555c86b63
Curtis Wong <cwong990@berkeley.edu>
2022-04-13 18:39:28 -0700

    frontend set up components and routes

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './Components/Home';
+import LogIn from './Components/Login';
+import SignUp from './Components/Signup';
+import Contact from './Components/Contact';
+import { Routes, route, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes> <Route path="/" element={<Homepage/>} /></Routes>
+      <Routes> <Route path="/login" element={<LogIn/>} /></Routes>
+      <Routes> <Route path="/signup" element={<SignUp/>} /></Routes>
+      <Routes> <Route path="/contact" element={<Contact/>} /></Routes>
     </div>
   );
 }
