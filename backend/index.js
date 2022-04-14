ead5d3a2406a74b4dcadf10258e3f992437e07ed
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-04-13 18:06:35 -0700

    implemented everything on Lesson 15 up to and not including 'Get LoggedIn User'

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const user = require('./routes/user');
+const InitiateMongoServer = require('./config/db');
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
+
+/**
+ * Router Middleware
+ * Router = /user/*
+ * Method - *
+ */
+app.use('/user', user);
+
+app.get('/', (req, res) => {
+    res.json({ message : 'API Working' });
+});
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server Started at PORT ${PORT}`);
+});
