641576e9eada6fd9b673e4f1b653bf6d126b84a9
vika-stukalova <90108074+vika-stukalova@users.noreply.github.com>
2022-04-14 23:27:10 -0700

    modified signup page to have first name, last name, email, and password

--- frontend/my-app/src/Components/SignUp/Signup.js
+++ frontend/my-app/src/Components/SignUp/Signup.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const SignUp = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <h1>
-            Sign Up!
-        </h1>
-    )
+    <div className="SignUp">
+        <Form onSubmit={handleSubmit}>
+            <Form.Group size = "lg" controlId="firstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                    autofocus
+                    type = "firstName"
+                    value = {firstName}
+                    onChange = {(d) => setFirstName(d.target.value)}
+                    />
+            </Form.Group>
+            <Form.Group size = "lg" controlId="LastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                    type = "lastName"
+                    value = {lastName}
+                    onChange = {(d) => setLastName(d.target.value)}
+                    />
+            </Form.Group>
+            <Form.Group size="lg" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    type = "email"
+                    value = {email}
+                    onChange = {(d) => setEmail(d.target.value)}
+                    />
+            </Form.Group>
+            <Form.Group size = "lg" controlId = "password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type = "password"
+                    value = {password}
+                    onChange = {(d) => setPassword(d.target.value)}
+                    />
+            </Form.Group>
+            <Button block size ="lg" type="submit" disabled={!validateForm()}>
+                Sign Up
+            </Button>
+        </Form>
+    </div>
+    );
 }
 
 export default SignUp;
