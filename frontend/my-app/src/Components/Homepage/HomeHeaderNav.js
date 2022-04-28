af17ba64165783b12798011227cb1b6a9be16e27
Curtis Wong <cwong990@berkeley.edu>
2022-04-27 20:51:36 -0700

    home header nav fix

--- frontend/my-app/src/Components/Homepage/HomeHeaderNav.js
+++ frontend/my-app/src/Components/Homepage/HomeHeaderNav.js
@@ -2,6 +2,8 @@ import './Home.css';
 import React, { useState, useEffect } from 'react';
 import AuthService from '../../Services/auth-service';
 import { Navigate, useNavigate } from "react-router-dom";
+import theme from '../../theme/theme';
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -10,7 +12,8 @@ import {
     ButtonGroup,
     Flex,
     Link,
-    Spacer
+    Spacer,
+    ChakraProvider
   } from '@chakra-ui/react';
 
 const HomeHeaderNav = () => {
@@ -33,31 +36,33 @@ const HomeHeaderNav = () => {
     };
 
     return (
-        <Flex>
-            <Breadcrumb className='pley-home-nav' spacing='24px' separator="">
-                <BreadcrumbItem className='pley-nav-link'>
-                    <BreadcrumbLink href='/'>Home</BreadcrumbLink>
-                </BreadcrumbItem>
-                {currentUser && (
+        <ChakraProvider theme={theme}>
+            <Flex>
+                <Breadcrumb className='pley-home-nav' spacing='24px' separator="">
                     <BreadcrumbItem className='pley-nav-link'>
-                    <BreadcrumbLink href='./search'>Search</BreadcrumbLink>
+                        <BreadcrumbLink href='/'>Home</BreadcrumbLink>
                     </BreadcrumbItem>
+                    {currentUser && (
+                        <BreadcrumbItem className='pley-nav-link'>
+                        <BreadcrumbLink href='./search'>Search</BreadcrumbLink>
+                        </BreadcrumbItem>
+                    )}
+                </Breadcrumb>
+                <Spacer/>
+                {currentUser ? (
+                    
+                    <ButtonGroup className="user-nav" spacing='6'>
+                    <Button onClick={() => logOut()} colorScheme='yellow' size='md' width={'200px'} color='#ffffff'>Sign Out</Button>
+                    <Link href="profile" color="#ffffff"><Button colorScheme='red' size='md' width={'200px'} color='#ffffff'>Profile</Button></Link>
+                    </ButtonGroup>
+                    
+                ) : (
+                    <ButtonGroup className="user-nav" spacing='6'>
+                    <Link href="login" color="#ffffff"><Button colorScheme='blue' size='md' width={'200px'} color='#ffffff'>Log In</Button></Link>
+                    </ButtonGroup>
                 )}
-            </Breadcrumb>
-            <Spacer/>
-            {currentUser ? (
-                
-                <ButtonGroup className="user-nav" spacing='6'>
-                <Button onClick={() => logOut()} colorScheme='yellow' size='md' width={'200px'} color='#ffffff'>Sign Out</Button>
-                <Link href="profile" color="#ffffff"><Button colorScheme='red' size='md' width={'200px'} color='#ffffff'>Profile</Button></Link>
-                </ButtonGroup>
-                
-            ) : (
-                <ButtonGroup className="user-nav" spacing='6'>
-                <Link href="login" color="#ffffff"><Button colorScheme='blue' size='md' width={'200px'} color='#ffffff'>Log In</Button></Link>
-                </ButtonGroup>
-            )}
-        </Flex>
+            </Flex>
+        </ChakraProvider>
     )
 }
 
--- frontend/my-app/src/theme/theme.js
+++ frontend/my-app/src/theme/theme.js
@@ -2,11 +2,12 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
     colors: {
-        brand: {
-            50: '#37447E',
-            100: '#FFD166'
-        }
-    }
-})
+        primary: "#37447E",
+        secondary: "#FFD166",
+        turquoise: "#158582",
+        teal: "#00C6C2",
+        turtle: "#324B4A"
+    },
+});
 
 export default theme;
