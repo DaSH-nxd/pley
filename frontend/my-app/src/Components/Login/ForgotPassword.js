55d996140b028029c9516d7a9d298a4f537c3912
Bryan Kim <bryanckim@berkeley.edu>
2022-04-28 05:19:22 -0700

    sign up changing password

--- frontend/my-app/src/Components/Login/ForgotPassword.js
+++ frontend/my-app/src/Components/Login/ForgotPassword.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import {
     Button,
     Modal,
@@ -24,10 +26,34 @@ const ForgotPassword = () => {
       )
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [overlay, setOverlay] = React.useState(<OverlayOne />)
+    const [email, setEmail] = useState("");
+    const [oldpass, setOldpass] = useState("");
+    const [newpass, setNewpass] = useState("");
 
+    
+    const changePass = async () => {
       
+      const data = {
+        "email": email,
+        "pass1": oldpass,
+        "pass2": newpass
+      };
+
+      axios
+      .put("http://localhost:3002/user/password",
+        data,
+        )
+        .then((res) => console.log(res))
+        .catch((err) => console.log("password change failed"));
+
+      return onClose();
+      //onCLose
+    }
+    
         const initialRef = React.useRef()
       
+    
+
         return (
           <>
             <Text onClick={() => {
@@ -49,22 +75,35 @@ const ForgotPassword = () => {
                 <ModalBody pb={6}>
                   <FormControl>
                     <FormLabel>Email</FormLabel>
-                    <Input ref={initialRef} placeholder='Enter your email' />
+                    <Input 
+                    ref={initialRef}
+                     placeholder='Enter your email' 
+                     value={email} 
+                     onChange = {(d) => setEmail(d.target.value)}
+                     />
                   </FormControl>
       
                   <FormControl mt={4}>
-                    <FormLabel>Old Password</FormLabel>
-                    <Input placeholder='New password' type={'password'}/>
+                    <FormLabel>New Password</FormLabel>
+                    <Input placeholder='New password'
+                           type={'password'}
+                           value={oldpass}
+                           onChange = {(d) => setOldpass(d.target.value)}
+                      />
                   </FormControl>
 
                   <FormControl mt={4}>
-                    <FormLabel>New Password</FormLabel>
-                    <Input placeholder='Retype new password' type={'password'}/>
+                    <FormLabel>Retype New Password</FormLabel>
+                    <Input value={newpass}  
+                           placeholder='Retype new password'
+                           type={'password'}
+                           onChange = {(d) => setNewpass(d.target.value)}
+                           />
                   </FormControl>
                 </ModalBody>
       
                 <ModalFooter>
-                  <Button colorScheme='blue' mr={3}>
+                  <Button onClick={() => changePass()} colorScheme='blue' mr={3}>
                     Send
                   </Button>
                   <Button onClick={onClose}>Cancel</Button>
